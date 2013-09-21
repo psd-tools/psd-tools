7852f0ec4e734e9fcbc8fd946be360e62801d2f2
a-e-m <alexemartyn@gmail.com>
2013-09-21 12:53:17 -0700

    Integrated CMYK branch, worked on ICC profiles
    CMYK and grayscale are now working in the same fork. I think everything
    now works correctly with ICC profiles, but I'm not sure enough about
    what the correct results should be.

--- src/psd_tools/user_api/pil_support.py
+++ src/psd_tools/user_api/pil_support.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, unicode_literals, division
 import warnings
 from psd_tools.utils import be_array_from_bytes
 from psd_tools.constants import Compression, ChannelID, ColorMode, ImageResourceID
+from psd_tools.icc_profiles import GrayProfile
 
 try:
     from PIL import Image, ImageCms
@@ -23,13 +24,19 @@ def extract_layer_image(decoded_data, layer_index):
     layers = decoded_data.layer_and_mask_data.layers
     layer = layers.layer_records[layer_index]
 
-    channels_data = layers.channel_image_data[layer_index]
+    channel_data = layers.channel_image_data[layer_index]
     size = layer.width(), layer.height()
-    channel_types = [info.id for info in layer.channels]
+    channel_ids = [info.id for info in layer.channels]
 
-    return _channels_data_to_PIL(
-        channels_data, channel_types, size,
-        decoded_data.header.depth, get_icc_profile(decoded_data))
+    return _channel_data_to_PIL(
+        channel_data = channel_data,
+        channel_ids = channel_ids,
+        color_mode = decoded_data.header.color_mode,  # XXX?
+        size = size,
+        depth = decoded_data.header.depth,
+        icc_profile = get_icc_profile(decoded_data)
+    )
+
 
 def extract_composite_image(decoded_data):
     """
@@ -39,38 +46,48 @@ def extract_composite_image(decoded_data):
     header = decoded_data.header
     size = header.width, header.height
     
+    channel_ids = None
     if header.color_mode == ColorMode.RGB:
-
         if header.number_of_channels == 3:
-            channel_types = [0, 1, 2]
+            channel_ids = [0, 1, 2]
         elif header.number_of_channels == 4:
-            channel_types = [0, 1, 2, -1]
-        else:
-            warnings.warn("This number of channels (%d) is unsupported for this color mode (%s)" % (
-                         header.number_of_channels, ColorMode.name_of(header.color_mode)))
-            return
-        
-    elif header.color_mode == ColorMode.GRAYSCALE:
+            channel_ids = [0, 1, 2, -1]
 
+    elif header.color_mode == ColorMode.CMYK:
+        if header.number_of_channels == 4:
+            channel_ids = [0, 1, 2, 3]
+        elif header.number_of_channels == 5:
+            # XXX: how to distinguish
+            # "4 CMYK + 1 alpha" and "4 CMYK + 1 spot"?
+            channel_ids = [-1, 0, 1, 2, 3] # XXX: unchecked!
+
+    elif header.color_mode == ColorMode.GRAYSCALE:
         if header.number_of_channels == 1:
-            channel_types = [3]
+            channel_ids = [0]
         elif header.number_of_channels == 2:
-            channel_types = [3, -1]
+            channel_ids = [0, -1]
             
     else:
         warnings.warn("Unsupported color mode (%s)" % ColorMode.name_of(header.color_mode))
         return
 
-    return _channels_data_to_PIL(
-        decoded_data.image_data,
-        channel_types,
-        size,
-        header.depth,
-        get_icc_profile(decoded_data),
+    if channel_ids is None:
+        warnings.warn("This number of channels (%d) is unsupported for this color mode (%s)" % (
+                     header.number_of_channels, header.color_mode))
+        return
+
+
+    return _channel_data_to_PIL(
+        channel_data = decoded_data.image_data,
+        channel_ids = channel_ids,
+        color_mode = header.color_mode,
+        size = size,
+        depth = header.depth,
+        icc_profile = get_icc_profile(decoded_data),
     )
 
 
-def _channels_data_to_PIL(channels_data, channel_types, size, depth, icc_profile):
+def _channel_data_to_PIL(channel_data, channel_ids, color_mode, size, depth, icc_profile): 
     if Image is None:
         raise Exception("This module requires PIL (or Pillow) installed.")
 
@@ -79,11 +96,11 @@ def _channels_data_to_PIL(channels_data, channel_types, size, depth, icc_profile
 
     bands = {}
 
-    for channel, channel_type in zip(channels_data, channel_types):
+    for channel, channel_id in zip(channel_data, channel_ids):
 
-        pil_band = channel_id_to_PIL(channel_type)
+        pil_band = channel_id_to_PIL(channel_id, color_mode)
         if pil_band is None:
-            warnings.warn("Unsupported channel type (%d)" % channel_type)
+            warnings.warn("Unsupported channel type (%d)" % channel_id)
             continue
 
         if channel.compression in [Compression.RAW, Compression.ZIP, Compression.ZIP_WITH_PREDICTION]:
@@ -111,44 +128,94 @@ def _channels_data_to_PIL(channels_data, channel_types, size, depth, icc_profile
 
         bands[pil_band] = im.convert('L')
 
-    mode = _get_mode(bands.keys())
-    merged_image = Image.merge(mode, [bands[band] for band in mode])
+    sRGB = ImageCms.createProfile('sRGB')
 
-    if icc_profile is not None and not 'L' in mode: # ICC profiles don't work with grayscale right now
-        display_profile = ImageCms.createProfile('sRGB') # XXX: ImageCms.get_display_profile()?
-        ImageCms.profileToProfile(merged_image, icc_profile, display_profile, inPlace=True)
+    if color_mode == ColorMode.RGB:
+        if 'A' in bands:
+            merged_image = Image.merge('RGBA', [bands[key] for key in 'RGBA'])
+        else:
+            merged_image = Image.merge('RGB', [bands[key] for key in 'RGB'])
+        output_profile = sRGB
+
+    elif color_mode == ColorMode.CMYK:
+        if 'A' in bands:
+            # CMYK with alpha channels is not supported by PIL/Pillow
+            # see https://github.com/python-imaging/Pillow/issues/257
+            del bands['A']
+            warnings.warn("CMYKA images are not supported; alpha channel is dropped")
+
+        merged_image = Image.merge('CMYK', [bands[key] for key in 'CMYK'])
+        # colors are inverted in Photoshop CMYK images; invert them back
+        merged_image = frombytes('CMYK', size, merged_image.tobytes(), 'raw', 'CMYK;I')
+        output_profile = sRGB
+
+    elif color_mode == ColorMode.GRAYSCALE:
+        if 'A' in bands:
+            merged_image = Image.merge('LA', [bands[key] for key in 'LA'])
+        else:
+            merged_image = Image.merge('L', [bands[key] for key in 'L'])
+        output_profile = GrayProfile
+            
+    else:
+        raise NotImplementedError()
+
+    mode_string = _get_mode(bands)
+
+    if icc_profile is not None:
+        try:
+            if color_mode == ColorMode.CMYK:
+                merged_image = ImageCms.profileToProfile(merged_image, icc_profile, output_profile, outputMode='RGB')
+            else:
+                ImageCms.profileToProfile(merged_image, icc_profile, output_profile, inPlace=True)
+        except ImageCms.PyCMSError as e:
+            warnings.warn(repr(e))
 
     return merged_image
 
 
-def channel_id_to_PIL(channel_id):
-    BANDS_MAP = {
-        ChannelID.RED: 'R',
-        ChannelID.GREEN: 'G',
-        ChannelID.BLUE: 'B',
-        ChannelID.GRAYSCALE: 'L',
-        ChannelID.TRANSPARENCY_MASK: 'A'
-    }
-    return BANDS_MAP.get(channel_id, None)
+def channel_id_to_PIL(channel_id, color_mode):
+    if ChannelID.is_known(channel_id):
+        if channel_id == ChannelID.TRANSPARENCY_MASK:
+            return 'A'
+        warnings.warn("Channel %s (%s) is not handled" % (channel_id, ChannelID.name_of(channel_id)))
+        return None
+
+    try:
+        assert channel_id >= 0
+        if color_mode == ColorMode.RGB:
+            return 'RGB'[channel_id]
+        elif color_mode == ColorMode.CMYK:
+            return 'CMYK'[channel_id]
+        elif color_mode == ColorMode.GRAYSCALE:
+            return 'L'[channel_id]
+
+    except IndexError:
+        # spot channel
+        warnings.warn("Spot channel %s is not handled" % channel_id)
+        return None
 
 
 def _get_mode(band_keys):
-    for mode in ['RGBA', 'RGB', 'L', 'LA']:
+    for mode in ['RGBA', 'RGB', 'L', 'LA', 'CMYK', 'CMYKA']:
         if set(band_keys) == set(mode):
             return mode
+
 
 def _from_8bit_raw(data, size):
     return frombytes('L', size, data, "raw", 'L')
 
+
 def _from_16bit_raw(data, size):
     im = frombytes('I', size, data, "raw", 'I;16B')
     return im.point(lambda i: i * (1/(256.0)))
+
 
 def _from_32bit_raw(data, size):
     pixels = be_array_from_bytes("f", data)
     im = Image.new("F", size)
     im.putdata(pixels, 255, 0)
     return im
+
 
 def get_icc_profile(decoded_data):
     """
@@ -167,6 +234,7 @@ def get_icc_profile(decoded_data):
         return None
 
     return icc_profile
+
 
 def apply_opacity(im, opacity):
     """
