d259a49a5a9a555a2fc3ee870df6320a2226bd5c
Mikhail Korobov <kmike84@gmail.com>
2014-07-18 07:02:38 +0600

    test file from GH-20

--- tests/test_layer_masks.py
+++ tests/test_layer_masks.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+import pytest
 from .utils import decode_psd
 
 
-def test_file_with_masks_is_parsed():
-    psd = decode_psd('masks.psd')
+@pytest.mark.parametrize('filename', ['masks.psd', 'masks2.psd'])
+def test_file_with_masks_is_parsed(filename):
+    psd = decode_psd(filename)
     for channels in psd.layer_and_mask_data.layers.channel_image_data:
         assert len(channels) >= 3
