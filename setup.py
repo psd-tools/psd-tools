e448dfd4098171ee7730903786cc7a27fe5c69ae
Mikhail Korobov <kmike84@gmail.com>
2015-12-24 05:10:35 +0500

    TST trying to fix CI

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Multimedia :: Graphics',
