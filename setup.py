e894a96c3241d14f3a5460a4f0609475e222386b
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2018-08-24 15:33:45 +0900

    Add py37 environment

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup_args = dict(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Multimedia :: Graphics',
