41f405324387b1f6245d174105e09eae9da435b7
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2019-01-24 15:55:49 +0900

    Fix python 2.7 syntax

--- src/psd_tools2/psd/color_mode_data.py
+++ src/psd_tools2/psd/color_mode_data.py
@@ -50,11 +50,21 @@ class ColorModeData(ValueElement):
 
     def interleave(self):
         import array
-        return b''.join(
-            array.array('B', [
-                self.value[i],
-                self.value[i + 256],
-                self.value[i + 512]
-            ]).tobytes()
-            for i in range(256)
-        )
+        if bytes == str:
+            return b''.join(
+                array.array('B', [
+                    ord(self.value[i]),
+                    ord(self.value[i + 256]),
+                    ord(self.value[i + 512])
+                ]).tostring()
+                for i in range(256)
+            )
+        else:
+            return b''.join(
+                array.array('B', [
+                    (self.value[i]),
+                    (self.value[i + 256]),
+                    (self.value[i + 512])
+                ]).tobytes()
+                for i in range(256)
+            )
