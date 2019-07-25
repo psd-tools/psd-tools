287170da655024d8c1c25b23a9691832c6b93e05
Kota Yamaguchi <KotaYamaguchi1984@gmail.com>
2019-07-25 17:34:05 +0900

    experimentally add commutative blending modes

--- tests/psd_tools/composer/test_blend.py
+++ tests/psd_tools/composer/test_blend.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 import pytest
 import logging
+import colorsys
+import numpy as np
+import os
+from psd_tools.composer.blend import rgb_to_hls, hls_to_rgb
 
 from ..utils import full_name
 from .test_composer import test_compose_quality
@@ -28,10 +32,45 @@ logger = logging.getLogger(__name__)
     ('blend-modes/exclusion.psd', ),
     ('blend-modes/subtract.psd', ),
     ('blend-modes/hard-mix.psd', ),
+    ('blend-modes/saturation.psd', ),
 ])
 def test_blend_quality(filename):
+    test_compose_quality(filename, threshold=0.02)
+
+@pytest.mark.parametrize(("filename", ), [
+    ('blend-modes/divide.psd', ),
+    ('blend-modes/hue.psd', ),
+    ('blend-modes/color.psd', ),
+    ('blend-modes/luminosity.psd', ),
+])
+@pytest.mark.xfail
+def test_blend_quality_xfail(filename):
     test_compose_quality(filename, threshold=0.02)
 
 
 def test_pass_through_blend():
     test_compose_quality('blend-modes/pass-through.psd')
+
+
+def test_rgb_to_hls():
+    r = np.random.rand(10, 10)
+    g = np.random.rand(10, 10)
+    b = np.random.rand(10, 10)
+    r[0, 0] = 0.
+    g[0, 0] = 0.
+    b[0, 0] = 0.
+    hls = rgb_to_hls(np.stack((r, g, b), axis=2))
+    ref = np.vectorize(colorsys.rgb_to_hls)(r, g, b)
+    for i in range(3):
+        assert np.allclose(hls[i], ref[i])
+
+
+def test_hls_to_rgb():
+    h = np.random.rand(10, 10)
+    l = np.random.rand(10, 10)
+    s = np.random.rand(10, 10)
+    s[0, 0] = 0.
+    rgb = hls_to_rgb(h, l, s)
+    ref = np.vectorize(colorsys.hls_to_rgb)(h, l, s)
+    for i in range(3):
+        assert np.allclose(rgb[:, :, i], ref[i])
