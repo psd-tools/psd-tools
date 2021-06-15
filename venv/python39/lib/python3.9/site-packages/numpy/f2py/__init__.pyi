631ba61eb70095e03c184243e5b359351703ff27
Stephen Neal <stephen.neal14@gmail.com>
2021-06-15 11:14:45 -0400

    CUT venv from vcs

--- venv/python39/lib/python3.9/site-packages/numpy/f2py/__init__.pyi
+++
@@ -1,7 +0,0 @@
-from typing import Any, List
-
-__all__: List[str]
-
-run_main: Any
-compile: Any
-f2py_testing: Any
--- venv/python39/lib/python3.9/site-packages/numpy/matrixlib/__init__.pyi
+++
@@ -1,8 +0,0 @@
-from typing import Any, List
-
-__all__: List[str]
-
-matrix: Any
-bmat: Any
-mat: Any
-asmatrix: Any
