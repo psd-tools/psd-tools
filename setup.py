ca12d8bd27557cadd40d53663b7c008019eca33b
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2022-01-17 15:10:26 +0900

    add cp310 support

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Multimedia :: Graphics',
         'Topic :: Multimedia :: Graphics :: Viewers',
         'Topic :: Multimedia :: Graphics :: Graphics Conversion',
