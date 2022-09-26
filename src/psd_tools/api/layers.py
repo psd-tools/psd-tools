8375e35c7b9363c4193654541185b64fb5253275
cromachina <82557197+cromachina@users.noreply.github.com>
2022-09-26 09:21:02 -0400

    Make invalidate_bbox actually private

--- src/psd_tools/api/layers.py
+++ src/psd_tools/api/layers.py
@@ -65,7 +65,7 @@ class Layer(object):
         """
         return self.tagged_blocks.get_data(Tag.LAYER_ID, -1)
 
-    def _invalidate_bbox(self):
+    def __invalidate_bbox(self):
         """
         Invalidate this layer's _bbox and any parents recursively to the root.
         """
@@ -86,7 +86,7 @@ class Layer(object):
 
     @visible.setter
     def visible(self, value):
-        self._invalidate_bbox()
+        self.__invalidate_bbox()
         self._record.flags.visible = bool(value)
 
     def is_visible(self):
@@ -154,7 +154,7 @@ class Layer(object):
 
     @left.setter
     def left(self, value):
-        self._invalidate_bbox()
+        self.__invalidate_bbox()
         w = self.width
         self._record.left = int(value)
         self._record.right = int(value) + w
@@ -170,7 +170,7 @@ class Layer(object):
 
     @top.setter
     def top(self, value):
-        self._invalidate_bbox()
+        self.__invalidate_bbox()
         h = self.height
         self._record.top = int(value)
         self._record.bottom = int(value) + h
