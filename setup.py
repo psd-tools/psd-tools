51a7a4a19ff8f979db6335c71ec870738fd18759
Kota Yamaguchi <yamaguchi_kota@cyberagent.co.jp>
2025-09-16 09:20:49 +0900

    Fix incorrect Py_LIMITED_API version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ extensions = [
         "psd_tools.compression._rle",
         ["src/psd_tools/compression/_rle.pyx"],
         extra_compile_args=["/d2FH4-"] if sys.platform == "win32" else [],
-        define_macros=[("Py_LIMITED_API", 0x03130000)]
+        define_macros=[("Py_LIMITED_API", 0x030D0000)]
         if sys.version_info >= (3, 13)
         else [],
         py_limited_api=True if sys.version_info >= (3, 13) else False,
